1c5e255fbaf8ec52adf4d0ef36e96d0f0136bb12
Ihar Levachkov <pokerlis@gmail.com>
2024-05-09 22:38:11 +0300

    feat: add folders structure

---
+++ src/app/app.ts
@@ -0,0 +1,26 @@
+import Registration from '../pages/registration';
+
+export default class App {
+  root: HTMLDivElement;
+
+  Registration: Registration;
+
+  registrationPage: HTMLDivElement;
+
+  constructor() {
+    this.Registration = new Registration();
+    this.registrationPage = this.Registration.getElement();
+    this.root = this.init();
+  }
+
+  private init() {
+    const element = document.createElement('div');
+    element.classList.add('root');
+    return element;
+  }
+
+  render() {
+    document.body.append(this.root);
+    this.root.append(this.registrationPage);
+  }
+}
