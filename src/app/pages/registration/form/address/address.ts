825f2478d4d77282c67621631d3ea1f3d43cbb80
Ihar Levachkov <pokerlis@gmail.com>
2024-05-11 00:21:13 +0300

    feat: add validation for fields in the registration form

--- src/app/pages/registration/form/address/address.ts
+++ src/app/pages/registration/form/address/address.ts
@@ -1,6 +1,7 @@
 import BaseComponent from '../../../../components/baseComponent';
 import Input from '../../../../components/controls/input';
 import creatInputWithLabel from '../../../../utils/creatInputWithLabel';
+import checkInputValue from '../../../../utils/checkInputValue';
 
 export default class RegAddress {
   element: HTMLFieldSetElement;
@@ -9,18 +10,30 @@ export default class RegAddress {
 
   inputStreet: BaseComponent<HTMLInputElement>;
 
+  massageErrorStreet: BaseComponent<HTMLParagraphElement>;
+
   inputCity: BaseComponent<HTMLInputElement>;
+
+  massageErrorCity: BaseComponent<HTMLParagraphElement>;
 
   inputPostalCode: BaseComponent<HTMLInputElement>;
 
+  massageErrorPostalCode: BaseComponent<HTMLParagraphElement>;
+
   inputCountry: BaseComponent<HTMLInputElement>;
 
+  massageErrorCountry: BaseComponent<HTMLParagraphElement>;
+
   constructor() {
-    this.legend = new BaseComponent<HTMLLegendElement>('legend', [], 'Address');
+    this.legend = new BaseComponent<HTMLLegendElement>('legend', ['massage-error'], 'Address');
     this.inputStreet = Input(['registration__input-street']);
+    this.massageErrorStreet = new BaseComponent<HTMLParagraphElement>('p', ['massage-error'], '');
     this.inputCity = Input(['registration__input-password']);
+    this.massageErrorCity = new BaseComponent<HTMLParagraphElement>('p', ['massage-error'], '');
     this.inputPostalCode = Input(['registration__input-name']);
+    this.massageErrorPostalCode = new BaseComponent<HTMLParagraphElement>('p', ['massage-error'], '');
     this.inputCountry = Input(['registration__input-lastname']);
+    this.massageErrorCountry = new BaseComponent<HTMLParagraphElement>('p', ['massage-error'], '');
     this.element = this.init();
   }
 
@@ -28,13 +41,71 @@ export default class RegAddress {
     const regAddress = document.createElement('fieldset');
     const legend = this.legend.getElement();
 
+    this.inputStreet.addListener('input', () =>
+      checkInputValue(
+        this.inputStreet.getElement(),
+        this.massageErrorStreet.getElement(),
+        1,
+        '[a-zA-Z0-9\\s\\-]{2,}',
+        'Must contain at least one character'
+      )
+    );
     const labelStreet = creatInputWithLabel(this.inputStreet.getElement(), 'Street:', '4 Washington Pl,', 'text');
-    const labelCity = creatInputWithLabel(this.inputCity.getElement(), 'City:', 'New-York', 'text');
-    const labelPostalCode = creatInputWithLabel(this.inputPostalCode.getElement(), 'Postal code:', 'A1B 2C3', 'text');
+    const msgErrStreet = this.massageErrorStreet.getElement();
+
+    this.inputCity.addListener('input', () =>
+      checkInputValue(
+        this.inputCity.getElement(),
+        this.massageErrorCity.getElement(),
+        1,
+        '[a-zA-Z]{2,}',
+        'Must contain at least one character and no special characters or numbers'
+      )
+    );
+    const labelCity = creatInputWithLabel(this.inputCity.getElement(), 'City:', 'New York', 'text');
+    const msgErrCity = this.massageErrorCity.getElement();
+
+    this.inputPostalCode.addListener('input', () =>
+      checkInputValue(
+        this.inputPostalCode.getElement(),
+        this.massageErrorPostalCode.getElement(),
+        4,
+        '[a-zA-Z0-9\\s]{5,}',
+        'Must follow the format for the country'
+      )
+    );
+    const labelPostalCode = creatInputWithLabel(
+      this.inputPostalCode.getElement(),
+      'Postal code:',
+      '12345 or A1B 2C3',
+      'text'
+    );
+    const msgErrPostalCode = this.massageErrorPostalCode.getElement();
+
+    this.inputCountry.addListener('input', () =>
+      checkInputValue(
+        this.inputCountry.getElement(),
+        this.massageErrorCountry.getElement(),
+        1,
+        '[A-Z]{2,}',
+        'Must be a valid country'
+      )
+    );
     const labelCountry = creatInputWithLabel(this.inputCountry.getElement(), 'Country:', 'US or RU', 'text');
+    const msgErrCountry = this.massageErrorCountry.getElement();
 
     regAddress.classList.add('registration__address');
-    regAddress.append(legend, labelStreet, labelCity, labelPostalCode, labelCountry);
+    regAddress.append(
+      legend,
+      labelStreet,
+      msgErrStreet,
+      labelCity,
+      msgErrCity,
+      labelPostalCode,
+      msgErrPostalCode,
+      labelCountry,
+      msgErrCountry
+    );
 
     return regAddress;
   }
--- src/app/pages/registration/form/profile/profile.ts
+++ src/app/pages/registration/form/profile/profile.ts
@@ -1,6 +1,7 @@
 import BaseComponent from '../../../../components/baseComponent';
 import Input from '../../../../components/controls/input';
 import creatInputWithLabel from '../../../../utils/creatInputWithLabel';
+import checkInputValue from '../../../../utils/checkInputValue';
 
 export default class RegProfile {
   element: HTMLFieldSetElement;
@@ -9,21 +10,36 @@ export default class RegProfile {
 
   inputEmail: BaseComponent<HTMLInputElement>;
 
+  massageErrorEmail: BaseComponent<HTMLParagraphElement>;
+
   inputPassword: BaseComponent<HTMLInputElement>;
+
+  massageErrorPassword: BaseComponent<HTMLParagraphElement>;
 
   inputName: BaseComponent<HTMLInputElement>;
 
+  massageErrorName: BaseComponent<HTMLParagraphElement>;
+
   inputLastName: BaseComponent<HTMLInputElement>;
 
+  massageErrorLastName: BaseComponent<HTMLParagraphElement>;
+
   inputDate: BaseComponent<HTMLInputElement>;
+
+  massageErrorDate: BaseComponent<HTMLParagraphElement>;
 
   constructor() {
     this.legend = new BaseComponent<HTMLLegendElement>('legend', [], 'Profile');
     this.inputEmail = Input(['registration__input-email']);
+    this.massageErrorEmail = new BaseComponent<HTMLParagraphElement>('p', ['massage-error'], '');
     this.inputPassword = Input(['registration__input-password']);
+    this.massageErrorPassword = new BaseComponent<HTMLParagraphElement>('p', ['massage-error'], '');
     this.inputName = Input(['registration__input-name']);
+    this.massageErrorName = new BaseComponent<HTMLParagraphElement>('p', ['massage-error'], '');
     this.inputLastName = Input(['registration__input-lastname']);
+    this.massageErrorLastName = new BaseComponent<HTMLParagraphElement>('p', ['massage-error'], '');
     this.inputDate = Input(['registration__input-date']);
+    this.massageErrorDate = new BaseComponent<HTMLParagraphElement>('p', ['massage-error'], '');
     this.element = this.init();
   }
 
@@ -32,13 +48,63 @@ export default class RegProfile {
     const legend = this.legend.getElement();
 
     const labelEmail = creatInputWithLabel(this.inputEmail.getElement(), 'Email:', 'example@email.com', 'email');
+    const msgErrEmail = this.massageErrorEmail.getElement();
+
+    this.inputPassword.addListener('input', () =>
+      checkInputValue(
+        this.inputPassword.getElement(),
+        this.massageErrorPassword.getElement(),
+        1,
+        '[a-z+A-Z+0-9+]{8,}',
+        'Minimum 8 characters, at least 1 uppercase letter, 1 lowercase letter, and 1 number'
+      )
+    );
     const labelPassword = creatInputWithLabel(this.inputPassword.getElement(), 'Password:', 'Example-123', 'password');
+    const msgErrPassword = this.massageErrorPassword.getElement();
+
+    this.inputName.addListener('input', () =>
+      checkInputValue(
+        this.inputName.getElement(),
+        this.massageErrorName.getElement(),
+        1,
+        '[a-zA-Z]{2,}',
+        'Must contain at least one character and no special characters or numbers'
+      )
+    );
     const labelName = creatInputWithLabel(this.inputName.getElement(), 'First Name:', 'Name', 'text');
+    const msgErrName = this.massageErrorName.getElement();
+
+    this.inputLastName.addListener('input', () =>
+      checkInputValue(
+        this.inputLastName.getElement(),
+        this.massageErrorLastName.getElement(),
+        1,
+        '[a-zA-Z]{2,}',
+        'Must contain at least one character and no special characters or numbers'
+      )
+    );
     const labelLastName = creatInputWithLabel(this.inputLastName.getElement(), 'Last Name:', 'Lastname', 'text');
+    const msgErrLastName = this.massageErrorLastName.getElement();
+
+    this.inputDate.getElement().setAttribute('max', '2006-01-01');
+    this.inputDate.getElement().setAttribute('min', '1920-01-01');
     const labelDate = creatInputWithLabel(this.inputDate.getElement(), 'Birth:', '', 'date');
+    const msgErrDate = this.massageErrorDate.getElement();
 
     regProfile.classList.add('registration__profile');
-    regProfile.append(legend, labelEmail, labelPassword, labelName, labelLastName, labelDate);
+    regProfile.append(
+      legend,
+      labelEmail,
+      msgErrEmail,
+      labelPassword,
+      msgErrPassword,
+      labelName,
+      msgErrName,
+      labelLastName,
+      msgErrLastName,
+      labelDate,
+      msgErrDate
+    );
 
     return regProfile;
   }
