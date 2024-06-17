2cfa429be5b0057e0e843b459e038db6df0960fa
Irina <myr-irina2@yandex.ru>
2024-06-17 21:11:17 +0300

    feat: add adaptive to product details

--- src/app/pages/about-us/profileCard/profileCard.ts
+++ src/app/pages/about-us/profileCard/profileCard.ts
@@ -2,7 +2,6 @@ import './profileCard.scss';
 import BaseComponent from '../../../components/baseComponent/baseComponent';
 import Layout from '../../../layout/layout';
 import { IProfile } from '../../../../data/team';
-// import '../../../../assets/igor.jpg';
 
 export default class ProfileCard extends Layout {
   profileCard: IProfile;
